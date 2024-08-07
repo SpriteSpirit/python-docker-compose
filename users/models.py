cdebf2d7f2706459d5db1b48502d0e5682854f9b
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-08-07 22:14:01 +0300

    init

---
+++ users/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django_countries.fields import CountryField
+
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    """ Пользователь """
+    username = None
+
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = PhoneNumberField(verbose_name='Телефон', default='+7', **NULLABLE)
+    country = CountryField(verbose_name='Страна', default='RU')
+    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Фото', **NULLABLE)
+    about_message = models.TextField(verbose_name='О себе', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
