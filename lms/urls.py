67e0b2b45a2247ee696e00d5c20adf61627baf37
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-08-07 23:09:10 +0300

    Добавлен generic для просмотра списка уроков

--- lms/urls.py
+++ lms/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path
 
 from lms.views import CourseViewSet
-from lms.views import LessonCreateAPIView
+from lms.views import LessonCreateAPIView, LessonListAPIView
 
 app_name = LmsConfig.name
 
@@ -11,5 +11,6 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-    path('lesson/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
 ] + router.urls
--- lms/views.py
+++ lms/views.py
@@ -15,3 +15,9 @@ class CourseViewSet(viewsets.ModelViewSet):
 class LessonCreateAPIView(generics.CreateAPIView):
     """ Создание урока """
     serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    """ Список уроков """
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
