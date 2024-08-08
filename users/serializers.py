ce8b3508b91817ba33117e159d1a78234a0e059e
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-08-08 23:25:09 +0300

    Настроена сериализатор пользователя для вывода платежей

--- users/serializers.py
+++ users/serializers.py
@@ -2,15 +2,18 @@ from rest_framework import serializers
 from users.models import User, Payment
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
 class PaymentSerializer(serializers.ModelSerializer):
-    """ Сериализатор платежей """
+    """ Сериализатор платежа """
 
     class Meta:
         model = Payment
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """ Сериализатор пользователя """
+    payments = PaymentSerializer(source='payment', many=True, read_only=True)
+
+    class Meta:
+        model = User
         fields = '__all__'
